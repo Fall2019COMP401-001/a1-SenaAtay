1e275ebfe408cf9b28159780f51b8c23b523c594
sena <sena@10.250.72.47>
2019-09-02 04:50:47 -0400

    Trying to make sure my code is updated.

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -2,13 +2,69 @@ package a1;
 
 import java.util.Scanner;
 
+
 public class A1Adept {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		int productsandprices;
+		int numberofpeople;
+		String firstname;
+		String lastname;
+		int numberofproductsperperson;
+		int numberofspecificproduct;
+		String theproduct;
+		int ind4product;
+		String biggestpersonfirstname = null;
+		String biggestpersonlastname = null;
+		String smallestpersonfirstname = null;
+		String smallestpersonlastname = null;
+		Double biggest = 0.0;
+		Double smallest = 100.0;
+		Double mtotalcost = 0.0;
 
-		// Your code follows here.
+		productsandprices = scan.nextInt();
+		String[] products = new String[productsandprices];
+		Double[] prices = new Double[productsandprices];
+		for(int i=0;i<productsandprices;i++) {
+			products[i] = scan.next();
+			prices[i] = scan.nextDouble();
+		}
+		numberofpeople = scan.nextInt();
+		for(int j=0;j<numberofpeople;j++) {
+			firstname = scan.next();
+			lastname = scan.next();
+			numberofproductsperperson = scan.nextInt();
+			Double totalcost = 0.0;
+			for(int k=0;k<numberofproductsperperson;k++) {
+				numberofspecificproduct = scan.nextInt();
+				theproduct = scan.next();
+				ind4product=0;
+				for (int ind=0; ind<products.length; ind++) {
+					if (products[ind].equals(theproduct))
+						ind4product=ind;	
+				}
+				Double priceofspecificproduct = prices[ind4product];
+				totalcost = totalcost + (numberofspecificproduct*priceofspecificproduct);
+			}
+			mtotalcost = mtotalcost + totalcost;
+
+				if (totalcost > biggest) {
+					biggestpersonfirstname = firstname;
+					biggestpersonlastname = lastname;
+					biggest = totalcost;
+				}
+				if (totalcost < smallest) {
+					smallestpersonfirstname = firstname;
+					smallestpersonlastname = lastname;
+					smallest = totalcost;
+				}
+			}
+		System.out.println("Biggest: " + biggestpersonfirstname + " "  + biggestpersonlastname + ": " + "(" + biggest + ")");
+		System.out.println("Smallest: " + smallestpersonfirstname + " "  + smallestpersonlastname + ": " + "(" + smallest + ")");
+		System.out.println("Average: " + mtotalcost/numberofpeople);
 		
+		}
 	}
-}
+
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,27 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		String fname;
+		String lname;
+		String pname;
+		int numberofproducts = 0;
+		int pnumber = 0;
+		double pcost = 0;
+		double totalcost;
 
-		// Your code follows here.
-		
+		int numberofpeople = scan.nextInt();
+		for(int i=0;i<numberofpeople;i++) {
+			fname = scan.next();
+			lname = scan.next();
+			totalcost = 0;
+			numberofproducts = scan.nextInt();
+			for(int a=0;a<numberofproducts;a++) {
+				pnumber = scan.nextInt();
+				pname = scan.next();
+				pcost = scan.nextDouble();
+				totalcost = totalcost + pnumber*pcost;
+			}	
+			System.out.println(fname.charAt(0) + ". " + lname + ": " + totalcost);
+		}	
 	}
 }
